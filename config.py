adb53a745abcf1bfeed29115c3bcd6befe87c338
Kit <77265081+YShengKit@users.noreply.github.com>
2023-12-19 11:44:24 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.crmkl0athd31.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "yeapshengkit-employee"
+customregion = "us-east-1"
+
